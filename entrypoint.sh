b1c4d576ceb554af9a4c08deddece9c67a2ac0a3
Daniel Glans <23437787+danielglans@users.noreply.github.com>
2021-07-12 10:54:10 +0200

    Update entrypoint.sh
    Cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ rsync --progress -avzh \
 
 if [ $? -eq 0 ]
 then
-	if [ $6 == true ]
+	if [[ $6 = true ]]
 	then
 	echo $'\n' "------ SET APPROPIATE PHP BIN -------------------" $'\n'
 	
@@ -40,8 +40,6 @@ then
 	php=php
 	
 	fi
-	
-	echo "daniel php - $php"
 
 	echo $'\n' "------ SYNC SUCCESSFUL! -----------------------" $'\n'
 	echo $'\n' "------ RELOADING PERMISSION -------------------" $'\n'
@@ -58,7 +56,7 @@ then
 	ssh -i /root/.ssh/id_rsa -t $1@$2 "cd $3 && $php artisan config:cache"
 	ssh -i /root/.ssh/id_rsa -t $1@$2 "cd $3 && $php artisan queue:restart"
 	
-	if [ $6 == true ]
+	if [[ $6 = true ]]
 	then
 	echo $'\n' "------ RELOAD LIGHTSPEED -------------------" $'\n'
 	
